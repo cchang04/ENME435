5a8145ba510deea6f685380cd066733fa1e7e2dc
cchang04 <cchang04@terpmail.umd.edu>
2026-04-22 19:34:29 -0400

    Update laser_alignment_image.py

--- laser_alignment_image.py
+++ laser_alignment_image.py
@@ -3,7 +3,7 @@ import cv2
 import time
 
 camera = Picamera2()
-config = camera.create_video_configuration(main={"format": "BGR888", "size": (1280, 720)})
+config = camera.create_video_configuration(main={"format": "RGB888", "size": (1280, 720)})
 camera.configure(config)
 camera.start()
 time.sleep(1)
