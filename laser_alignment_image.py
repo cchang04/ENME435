fc8a38007fecab2c6aa62da92f10982c0ed3f935
cchang04 <cchang04@terpmail.umd.edu>
2026-04-21 20:25:42 -0400

    Update laser_alignment_image.py

--- laser_alignment_image.py
+++ laser_alignment_image.py
@@ -8,7 +8,6 @@ camera.configure(config)
 camera.start()
 time.sleep(1)
 
-# Set up VideoWriter to save annotated frames
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
 out = cv2.VideoWriter("output.mp4", fourcc, 25, (1280, 720))
 
@@ -18,16 +17,24 @@ while True:
     image = camera.capture_array()
     image = cv2.flip(image, -1)
 
-    # crosshairs
+    # draw lines first
     cv2.line(image, (640, 0), (640, 720), (0, 150, 150), 1)
     cv2.line(image, (0, 360), (1280, 360), (0, 150, 150), 1)
 
-    # green vertical alignment lines
     for i in range(50, 1300, 50):
-        cv2.line(image, (i, 0), (i, 720), (0, 150, 0), 3)
+        cv2.line(image, (i, 0), (i, 720), (0, 255, 0), 8)
 
-    # write annotated frame to video file
+    # then write annotated frame to file
     out.write(image)
 
+    # then display
     cv2.imshow("Image", image)
     key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        break
+
+camera.stop()
+out.release()
+cv2.destroyAllWindows()
+print("Saved to output.mp4")
