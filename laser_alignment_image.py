0527f843eb2f16ab06933981a4b3ff0826d826d0
cchang04 <cchang04@terpmail.umd.edu>
2026-04-22 19:33:11 -0400

    Update laser_alignment_image.py

--- laser_alignment_image.py
+++ laser_alignment_image.py
@@ -6,17 +6,7 @@ camera = Picamera2()
 config = camera.create_video_configuration(main={"format": "BGR888", "size": (1280, 720)})
 camera.configure(config)
 camera.start()
-time.sleep(2)
-
-camera.set_controls({
-    "AwbEnable": False,
-    "AwbMode": 0,
-    "ColourGains": (3.0, 0.5),
-    "ExposureTime": 30000,
-    "AnalogueGain": 6.0
-})
-
-time.sleep(2)
+time.sleep(1)
 
 print("Press 'q' to stop")
 
